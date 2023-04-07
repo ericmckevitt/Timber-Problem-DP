ca42b71f42c39b306bb074c4ab03bb487c1554a8
Eric McKevitt <erictmckevitt@gmail.com>
2023-04-06 19:36:52 -0600

    working dp function

--- dp_solution.py
+++ dp_solution.py
@@ -1,16 +1,3 @@
-
-
-# def T(i: int, j: int) -> int:
-    
-#     # Use lambda function to set values to None where i > j
-#     valid_cell = lambda i, j: None if i > j else False
-    
-#     # Create dp table i rows by j columns
-#     dp_table = [[valid_cell(row, col) for col in range(j) ] for row in range(i) ]
-    
-#     for row in dp_table:
-#         print(row)
-
 # tree = [None, 5, 6, 9, 7]
 tree = [None, 6, 5, 7, 6, 4]
 n = len(tree) - 1
@@ -23,29 +10,33 @@ def print_table(table):
 
 def T(i: int, j: int) -> int:
     
-    # Use lambda function to set values to None where i > j
-    valid_cell = lambda i, j: None if i > j else False
-    
     # Create dp table i rows by j columns
-    dp_table = [[0 for _ in range(n) ] for _ in range(n) ]
-    
-    # Apply the lambda function to each index of the dp_table
-    for row in range(n):
-        for col in range(n):
-            dp_table[row][col] = valid_cell(row, col)
-            
-    # Apply base cases
-    for row in range(n):
+    dp_table = [[None for _ in range(n) ] for _ in range(n) ]
+        
+    # loop over the table from bottom row to top row
+    for row in range(n - 1, -1, -1):
         for col in range(n):
             
-            # Base case 1:
+            # i must be less than j
+            if row > col:
+                continue
+            
+            # Base case 1: l_i if i == j
             if row == col:
                 dp_table[row][col] = tree[row + 1] # +1 bc 1-indexed
+                continue
                 
-            # Base case 2: 
+            # Base case 2: max(l_i, l_j) if j = i + 1 
             if col == row + 1:
                 dp_table[row][col] = max(tree[row + 1], tree[col + 1])
-    
+                continue
+            
+            # Recurrence relation
+            dp_table[row][col] = max(tree[row+1] + min(dp_table[row+2][col], dp_table[row+1][col-1]),
+                                        tree[col+1] + min(dp_table[row+1][col-1], dp_table[row][col-2]))
+            
+        print()
     print_table(dp_table)
+    print()
     
 T(5, 5)
